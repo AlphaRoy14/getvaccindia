f8eccabc7f6f0e0a0697a63f44305a2340d7ce60
Arindaam Roy <alpharoy14@gmail.com>
2021-05-31 19:23:22 +0530

    🔧Update config settings

---
+++ backend/app/core/config.py
@@ -0,0 +1,31 @@
+import secrets
+from typing import Any, Dict, List, Optional, Union
+
+from pydantic import AnyHttpUrl, BaseSettings, EmailStr, HttpUrl, validator
+from ipaddress import IPv4Address, collapse_addresses
+
+
+class Settings(BaseSettings):
+    API_V1_STR: str = "/api/v1"
+    SECRET_KEY: str = secrets.token_urlsafe(32)
+
+    BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = []
+
+    @validator("BACKEND_CORS_ORIGINS", pre=True)
+    def assemble_cors_origins(cls, v: Union[str, List[str]]) -> Union[List[str], str]:
+        if isinstance(v, str) and not v.startswith("["):
+            return [i.strip() for i in v.split(",")]
+        elif isinstance(v, (list, str)):
+            return v
+        raise ValueError(v)
+
+    PROJECT_NAME: str
+    MONGODB_URL: str
+    DB_NAME: str
+    DOCUMENT: str
+
+    class Config:
+        case_sensitive = True
+
+
+settings = Settings(_env_file=".env")
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,39 @@
+import logging
+
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+from starlette.routing import Host
+
+from api.api_v1.api import api_router
+from db.mongodb_utils import connect_to_mongo, close_mongo_connection
+from core.config import settings
+
 import uvicorn
 
-if __name__ == 'main':
-    uvicorn.run("app.api:run", host = "0.0.0.0", port = 8000, reload = True)
+logging.basicConfig(
+    filename="fastapi.log",
+    format="[%(asctime)s] — %(name)s — %(levelname)s — %(funcName)s:%(lineno)d — %(message)s",
+    level=logging.DEBUG,
+)
+app = FastAPI(
+    title=settings.PROJECT_NAME,
+    openapi_url=f"{settings.API_V1_STR}/openapi.json",
+    description="Apis to subscribe and unsubscribe users from the mailing list",
+)
+app.include_router(api_router, prefix=settings.API_V1_STR)
 
+# 😇 Set all cors enabled origins
+if settings.BACKEND_CORS_ORIGINS:
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+app.add_event_handler("startup", connect_to_mongo)
+app.add_event_handler("shutdown", close_mongo_connection)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
