06de6bec602402d629ae0e685e4547bd830d2849
Arindaam Roy <alpharoy14@gmail.com>
2021-05-31 19:22:08 +0530

    🔧Add utils for db

--- backend/app/db/mongodb.py
+++ backend/app/db/mongodb.py
@@ -9,8 +9,10 @@ logger = logging.getLogger(__name__)
 class DataBase:
     client: AsyncIOMotorClient = None
 
+
 db = DataBase()
+
 
 async def get_db() -> AsyncIOMotorDatabase:
     """We need to only connect to a single database 🙃"""
-    return db.client[settings.DB_NAME]
+    return db.client
--- backend/app/db/mongodb_utils.py
+++ backend/app/db/mongodb_utils.py
@@ -5,10 +5,12 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from db.mongodb import db
 from core.config import settings
 
+
 async def connect_to_mongo():
     logging.info("Connecting to mongo...")
     db.client = AsyncIOMotorClient(str(settings.MONGODB_URL))
-    logging.info('DB connected!')
+    logging.info("DB connected!")
+
 
 async def close_mongo_connection():
     logging.info("Closing DB connection...")
