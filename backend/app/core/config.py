8aff7b7bc29ba7b633b3b034b9eafa1968bd3f17
Arindaam Roy <alpharoy14@gmail.com>
2021-05-31 21:38:45 +0530

    Update cors settings

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -9,7 +9,7 @@ class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
     SECRET_KEY: str = secrets.token_urlsafe(32)
 
-    BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = []
+    BACKEND_CORS_ORIGINS: List[AnyHttpUrl,str] = []
 
     @validator("BACKEND_CORS_ORIGINS", pre=True)
     def assemble_cors_origins(cls, v: Union[str, List[str]]) -> Union[List[str], str]:
