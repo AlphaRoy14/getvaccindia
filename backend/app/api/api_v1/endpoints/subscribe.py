7c5c1afb085666a716d7ce7a786d615f7f5bf715
Arindaam Roy <alpharoy14@gmail.com>
2021-08-30 07:17:01 +0530

    Update endpoint docscrings for subs

--- backend/app/api/api_v1/endpoints/subscribe.py
+++ backend/app/api/api_v1/endpoints/subscribe.py
@@ -17,27 +17,6 @@ logger.addHandler(logging.StreamHandler())
 router = APIRouter()
 
 
-@router.get("/email")
-async def get_email():
-    """
-    Just a test api for mailing service
-
-    """
-    raise HTTPException(
-        status_code=status.HTTP_501_NOT_IMPLEMENTED, detail="just a test api"
-    )
-    await send_confirmation_email(
-        email=["alpharoy14@gmail.com"],
-        template_data={
-            "name": "Arindaam",
-            "state": "west bengal",
-            "age": 23,
-            "vaccine doze": 2,
-        },
-    )
-    return {"message": "sent"}
-
-
 @router.post(
     "/subscribe",
     response_model=schemas.SubscriberResponseModel,
@@ -84,7 +63,7 @@ async def unsubscribe(id: str, db: AsyncIOMotorClient = Depends(get_db)):
 @router.get("/getAll", deprecated=True)
 async def get_all_subs(db: AsyncIOMotorClient = Depends(get_db)):
     """
-    Get all the subscribed users for testing
+    Get all the subscribed users for testing the api
     """
     try:
         subs = await crud.get_all_subscribers(db)
@@ -99,6 +78,7 @@ async def get_all_subs(db: AsyncIOMotorClient = Depends(get_db)):
     summary="Trigger email for testing",
 )
 async def trigger_email():
+    """ A test api to send email to all subscribed users """
     try:
         await run_mail_notif_task()
         return JSONResponse(status_code=status.HTTP_200_OK, content={"data": "nice"})
@@ -110,10 +90,26 @@ async def trigger_email():
 async def run_worker():
     try:
         await run_mail_notif_task()
-        # await format_and_send_email_worker.delay()
         return JSONResponse(
             status_code=status.HTTP_200_OK, content={"Data": "added to the queue"}
         )
     except Exception as e:
         logger.exception(e)
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+@router.get("/email")
+async def get_email():
+    """
+    A api for testing the mailing service
+    """
+    await send_confirmation_email(
+        email=["test@example.com"],
+        template_data={
+            "name": "User",
+            "state": "west bengal",
+            "age": 22,
+            "vaccine doze": 2,
+        },
+    )
+    return {"message": "sent"}
